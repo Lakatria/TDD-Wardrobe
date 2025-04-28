464a5858ac875b98973f25f36bb2bd33d3bc1d49
Lakatria <jaymienrodriguez@gmail.com>
2025-04-28 15:50:24 -0400

    test: write 50cm failing test

---
+++ src/test/java/com/example/project/WardrobeTests.java
@@ -0,0 +1,17 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class WardrobeTests {
+	@Test
+	void testSize50() {
+		Wardrobe w = new Wardrobe();
+		int size = w.getSize();
+		assertEquals(50, size);
+	}
+}
