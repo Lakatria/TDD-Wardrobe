b6535ae2a3b5d4228391e94ba88fe1d29a9e155b
Lakatria <jaymienrodriguez@gmail.com>
2025-04-28 16:03:53 -0400

    clean: add beforeeach to reduce duplicate code

--- src/test/java/com/example/project/WardrobeTests.java
+++ src/test/java/com/example/project/WardrobeTests.java
@@ -2,12 +2,19 @@ package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class WardrobeTests {
+    private Wardrobe w;
+
+    @BeforeEach
+    void setUp(){
+        w = new Wardrobe();
+    }
+
     @Test
     void testSize50() {
-        Wardrobe w = new Wardrobe();
         w.setSize(50);
         int size = w.getSize();
         assertEquals(50, size);
@@ -15,7 +22,6 @@ class WardrobeTests {
 
     @Test
     void testSize75() {
-        Wardrobe w = new Wardrobe();
         w.setSize(75);
         int size = w.getSize();
         assertEquals(75, size);
